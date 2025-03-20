28ee4c9f2c4d6a35420dbd3fe3de1b79a2aca9b4
CollinsGichuki <collins.gichuki@bonial.com>
2025-03-20 13:27:17 +0100

    feat(WeatherForecast): add scrolling to current time

--- app/src/main/java/com/example/mvi_weatherapp/presentation/views/WeatherForecast.kt
+++ app/src/main/java/com/example/mvi_weatherapp/presentation/views/WeatherForecast.kt
@@ -4,14 +4,15 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -24,6 +25,8 @@ fun WeatherForecast(
     state: WeatherState,
     modifier: Modifier = Modifier
 ) {
+    val listState = rememberLazyListState()
+
     state.weatherInfo?.weatherDataPerDay?.get(0)?.let { data ->
         Column(
             modifier = modifier
@@ -33,7 +36,9 @@ fun WeatherForecast(
             Text(text = "Today", fontSize = 20.sp, color = Color.White)
 
             Spacer(modifier = Modifier.height(16.dp))
-            LazyRow(content = {
+            LazyRow(
+                state = listState
+            ) {
                 items(data) { weatherData ->
                     HourlyWeatherDisplay(
                         weatherData = weatherData,
@@ -42,7 +47,14 @@ fun WeatherForecast(
                             .padding(horizontal = 16.dp)
                     )
                 }
-            })
+            }
+
+            LaunchedEffect(state.weatherInfo.weatherDataPerDay) {
+                val currentTime = state.weatherInfo.weatherDataPerDay.values
+                    .flatten()
+                    .find { it.time == state.weatherInfo.currentWeatherData?.time }
+                listState.animateScrollToItem(data.indexOf(currentTime))
+            }
         }
     }
 }
